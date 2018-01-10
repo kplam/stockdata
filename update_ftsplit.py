4adf7eff4861341ef0854550414aa56b98c4a570
kplam <admin@kplam.com>
2018-01-10 21:37:55 +0800

    V1.1.0

--- update_ftsplit.py
+++ update_ftsplit.py
@@ -12,7 +12,7 @@ from kpfunc.getdata import *
 from kpfunc.spyder import myspyder
 
 def split_szfh(iLong,Quarter,proxy=0):
-    today = datetime.date.today()-datetime.timedelta(days=5)
+    today = datetime.date.today() #-datetime.timedelta(days=5)
     try:
         url = "http://dcfm.eastmoney.com/EM_MutiSvcExpandInterface/api/js/get?type=DCSOBS&token=70f12f2f4f091e459a279469fe49eca5&p=1&ps=%s&sr=-1&st=SZZBL&filter=(ReportingPeriod=^%s^)" % (
         iLong, Quarter)
